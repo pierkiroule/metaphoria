34f2c1c7d6271af47853eeeecafdb6c38f5141bb
Garnier Pierre-Henri <39243998+pierkiroule@users.noreply.github.com>
2025-12-14 13:52:03 +0100

    Add app-level error boundary for blank screen cases

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AppBoundary } from './components/AppBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppBoundary>
+      <App />
+    </AppBoundary>
   </React.StrictMode>,
 )
